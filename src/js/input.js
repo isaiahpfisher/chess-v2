5e1ae075b3b6ff089042f79f58d373fbffb71948
isaiahpfisher <isaiahpfisher@gmail.com>
2023-11-15 12:51:18 -0600

    More comments

--- src/js/input.js
+++ src/js/input.js
@@ -1,19 +1,50 @@
+// adds event listeners to every chess piece on the board for drag-and-drop functionality
 document.querySelectorAll(".chessboard .row .cell").forEach(img => {
   img.addEventListener("drop", drop);
-  img.addEventListener("dragover", allowDrop);
-  img.addEventListener("dragstart", drag);
+  img.addEventListener("dragover", dragOver);
+  img.addEventListener("dragleave", dragLeave);
+  img.addEventListener("dragstart", dragStart);
 })
 
-function allowDrop(e) {
+/**
+ * Prevents default behavior when a chess piece is dragged over.
+ * Runs when a chess piece is dragged over e.target
+ * @param {Event} e
+ * @returns {void}
+ */
+function dragOver(e) {
   e.preventDefault();
-  e.handled = true;
+  // e.target.closest(".cell").classList.add(""); TODO: Add a class here to style spaces when hovered over
 }
 
-function drag(e) {
+/**
+ * Prevents default behavior when a dragged chess piece leaves a space on the chessboard.
+ * Runs when a dragged chess piece leaves a space on the chessboard.
+ * @param {Event} e
+ * @returns {void}
+ */
+function dragLeave(e) {
+  e.preventDefault();
+  // e.target.closest(".cell").remove.add(""); TODO: Remove a class here to style spaces when hovered over
+}
+
+/**
+ * Associates the id of the origin cell with the dragged element.
+ * Runs when an element is first dragged
+ * @param {Event} e
+ * @returns {void}
+ */
+function dragStart(e) {
   e.dataTransfer.setData("text", e.target.closest(".cell").id);
   e.dataTransfer.effectAllowed = "move";
 }
 
+/**
+ * Initiates a new move on the board.
+ * Runs when a chess piece is dropped on e.target cell.
+ * @param {Event} e
+ * @returns {void}
+ */
 function drop(e) {
   e.preventDefault();
   let id = e.dataTransfer.getData("text");
