e099ed1c1ff5660b79184850f15de1a952c7ce9e
isaiahpfisher <isaiahpfisher@gmail.com>
2023-11-17 12:41:02 -0600

    Empty.move()

--- src/js/classes/pieces/Empty.js
+++ src/js/classes/pieces/Empty.js
@@ -2,11 +2,10 @@ import { EMPTY, EMPTY_COLOR } from "../Board.js";
 import { Piece } from "../Piece.js";
 
 /**
- * Represents an empty space on a chessboard. 
+ * Represents an empty space on a chessboard.
  * Derived class of Piece.
  */
 export class Empty extends Piece {
-
   /**
    * @param {number} row - The row of the Empty space on the Board (0 - 7).
    * @param {number} col - The column of the Empty space on the Board (0 - 7).
@@ -16,15 +15,23 @@ export class Empty extends Piece {
     this.imgSrc = "";
   }
 
-   /**
+  /**
    * Checks if an Empty space can make the given move (always false)
    * @param {Array} grid - the board layout before the given move
    * @param {string} originId - id of starting space (e.g. A2 or H8)
    * @param {string} destId - id of ending space (e.g. A2 or H8)
    * @returns {boolean}
    */
-   isValidMove(grid, originId, destId) {
+  isValidMove(grid, originId, destId) {
     return false;
   }
 
+  /**
+   * Does piece-specific move actions for the Empty piece (does nothing)
+   * @param {Array} grid - The grid representing the game board.
+   * @param {string} originId - The ID of the piece's current position.
+   * @param {string} destId - The ID of the destination position.
+   * @returns {void}
+   */
+  move(grid, originId, destId) {}
 }
