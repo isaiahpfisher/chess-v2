f7112d8f9ddfd35cad72604ad203e831d1c90734
isaiahpfisher <isaiahpfisher@gmail.com>
2023-11-17 11:10:46 -0600

    Queen.isValidMove()

--- src/js/classes/pieces/Queen.js
+++ src/js/classes/pieces/Queen.js
@@ -2,11 +2,10 @@ import { QUEEN } from "../Board.js";
 import { Piece } from "../Piece.js";
 
 /**
- * Represents a queen on a chessboard. 
+ * Represents a queen on a chessboard.
  * Derived class of Piece.
  */
 export class Queen extends Piece {
-
   /**
    * @param {string} color - The color of the Queen (either BLACK or WHITE).
    * @param {number} row - The row of the Queen on the Board (0 - 7).
@@ -17,14 +16,27 @@ export class Queen extends Piece {
     this.imgSrc = `./assets/${color}-queen.svg`; // Set the image source for the Queen based on its color
   }
 
-   /**
+  /**
    * Checks if a Queen can make the given move
    * @param {Array} grid - the board layout before the given move
    * @param {string} originId - id of starting space (e.g. A2 or H8)
    * @param {string} destId - id of ending space (e.g. A2 or H8)
    * @returns {boolean}
    */
-   isValidMove(grid, originId, destId) {
-    return true;
+  isValidMove(grid, originId, destId) {
+    let validMove;
+
+    let startCoordinates = Piece.getCoordinates(originId);
+    let endCoordinates = Piece.getCoordinates(destId);
+
+    let rowDiff = Math.abs(startCoordinates.row - endCoordinates.row);
+    let colDiff = Math.abs(startCoordinates.col - endCoordinates.col);
+
+    // moved non-linearly
+    if (rowDiff != colDiff && !(rowDiff == 0 || colDiff == 0)) {
+      validMove = false;
+    }
+
+    return validMove;
   }
 }
