8aaaa8d7ec648cceae8981a87eaa79dd2104f13a
isaiahpfisher <isaiahpfisher@gmail.com>
2023-11-17 11:09:50 -0600

    Pawn.isValidMove()

--- src/js/classes/pieces/Pawn.js
+++ src/js/classes/pieces/Pawn.js
@@ -2,11 +2,10 @@ import { PAWN } from "../Board.js";
 import { Piece } from "../Piece.js";
 
 /**
- * Represents a pawn on a chessboard. 
+ * Represents a pawn on a chessboard.
  * Derived class of Piece.
  */
 export class Pawn extends Piece {
-
   /**
    * @param {string} color - The color of the Pawn (either BLACK or WHITE).
    * @param {number} row - The row of the Pawn on the Board (0 - 7).
@@ -15,6 +14,7 @@ export class Pawn extends Piece {
   constructor(color, row, col) {
     super(PAWN, color, row, col);
     this.imgSrc = `./assets/${color}-pawn.svg`; // Set the image source for the Pawn based on its color
+    this.enPassant = false;
   }
 
   /**
@@ -25,6 +25,50 @@ export class Pawn extends Piece {
    * @returns {boolean}
    */
   isValidMove(grid, originId, destId) {
-    return true;
+    let validMove = false;
+    let direction = this.color == WHITE ? WHITE_DIRECTION : BLACK_DIRECTION;
+
+    let startCoordinates = Piece.getCoordinates(originId);
+    let endCoordinates = Piece.getCoordinates(destId);
+
+    let rowDiff = Math.abs(startCoordinates.row - endCoordinates.row);
+    let colDiff = Math.abs(startCoordinates.col - endCoordinates.col);
+
+    // moved more than one column to the side
+    if (colDiff > 1 || (colDiff > 0 && rowDiff > 1)) {
+      validMove = false;
+    }
+    // tried to go backwards
+    else if (
+      endCoordinates.row * direction <
+      startCoordinates.row * direction
+    ) {
+      validMove = false;
+    }
+    // tried to move sideways
+    else if (startCoordinates.row == endCoordinates.row) {
+      validMove = false;
+    }
+    // tried to move more than one/two spaces forward
+    else if (rowDiff > 1 + !this.hasMoved) {
+      validMove = false;
+    }
+    // tried to move forward into enemny
+    else if (
+      startCoordinates.col == endCoordinates.col &&
+      !grid[endCoordinates.row][endCoordinates.col].isEmpty()
+    ) {
+      validMove = false;
+    }
+    // tried to move diagonallly (without taking enemy or en passant)
+    else if (
+      startCoordinates.row != endCoordinates.row &&
+      grid[endCoordinates.row][endCoordinates.col].isEmpty() &&
+      !grid[endCoordinates.row - direction][endCoordinates.col].enPassant
+    ) {
+      validMove = false;
+    }
+
+    return validMove;
   }
 }
